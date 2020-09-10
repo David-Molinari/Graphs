b2701378d533388cdfb192870ec620b5e3dc73f8
David Molinari <david.m.molinari@gmail.com>
2020-09-10 15:25:16 -0500

    update

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -100,7 +100,29 @@ class Graph:
 
         This should be done using recursion.
         """
-        pass  # TODO
+        # pass  # TODO
+
+        search_tracker = {}
+        parent_tracker = {}
+
+        def DFS_visit(vert):
+            search_tracker[vert] = 1
+            print(vert)
+
+            for n in self.get_neighbors(vert):
+                if search_tracker[n] == 0:
+                    parent_tracker[n] = vert
+                    DFS_visit(n)
+
+            search_tracker[vert] = 2
+
+        for v in self.vertices:
+            search_tracker[v] = 0
+            parent_tracker[v] = None
+
+        for v in self.vertices:
+            if search_tracker[v] == 0:
+                DFS_visit(v)
 
     def bfs(self, starting_vertex, destination_vertex):
         """
@@ -108,7 +130,43 @@ class Graph:
         starting_vertex to destination_vertex in
         breath-first order.
         """
-        pass  # TODO
+        # pass  # TODO
+
+        search_tracker = {}
+        path = []
+        parents = {}
+
+        for v in self.vertices:
+            search_tracker[v] = 0
+
+        q = Queue()
+
+        search_tracker[starting_vertex] = 1
+        q.enqueue(starting_vertex)
+
+        while q.isEmpty() is not True:
+            u = q.getOne(0)
+            if u == destination_vertex:
+                parent = u
+                while parent != -1:
+                    path.insert(0, parent)
+                    check = parents.get(parent)
+                    if check:
+                        parent = parents[parent]
+                    else:
+                        parent = -1
+                return path
+            
+            for v in self.get_neighbors(u):
+                if search_tracker[v] == 0:
+                    search_tracker[v] = 1
+                    parents[v] = u
+                    q.enqueue(v)
+            
+            q.dequeue()
+            search_tracker[u] = 2
+
+
 
     def dfs(self, starting_vertex, destination_vertex):
         """
@@ -116,7 +174,41 @@ class Graph:
         starting_vertex to destination_vertex in
         depth-first order.
         """
-        pass  # TODO
+        # pass  # TODO
+
+        search_tracker = {}
+        parent_tracker = {}
+        path = []
+
+        def DFS_visit(vert):
+            search_tracker[vert] = 1
+            print(vert)
+
+            for n in self.get_neighbors(vert):
+                if search_tracker[n] == 0:
+                    parent_tracker[n] = vert
+                    DFS_visit(n)
+
+            search_tracker[vert] = 2
+
+        for v in self.vertices:
+            search_tracker[v] = 0
+            parent_tracker[v] = None
+
+        for v in self.vertices:
+            if v == destination_vertex:
+                parent = v
+                while parent != -1:
+                    path.insert(0, parent)
+                    check = parent_tracker.get(parent)
+                    if check:
+                        parent = parent_tracker[parent]
+                    else:
+                        parent = -1
+                return path
+            if search_tracker[v] == 0:
+                DFS_visit(v)
+
 
     def dfs_recursive(self, starting_vertex, destination_vertex):
         """
@@ -126,7 +218,39 @@ class Graph:
 
         This should be done using recursion.
         """
-        pass  # TODO
+        # pass  # TODO
+        search_tracker = {}
+        parent_tracker = {}
+        path = []
+
+        def DFS_visit(vert):
+            search_tracker[vert] = 1
+            print(vert)
+
+            for n in self.get_neighbors(vert):
+                if search_tracker[n] == 0:
+                    parent_tracker[n] = vert
+                    DFS_visit(n)
+
+            search_tracker[vert] = 2
+
+        for v in self.vertices:
+            search_tracker[v] = 0
+            parent_tracker[v] = None
+
+        for v in self.vertices:
+            if v == destination_vertex:
+                parent = v
+                while parent != -1:
+                    path.insert(0, parent)
+                    check = parent_tracker.get(parent)
+                    if check:
+                        parent = parent_tracker[parent]
+                    else:
+                        parent = -1
+                return path
+            if search_tracker[v] == 0:
+                DFS_visit(v)
 
 if __name__ == '__main__':
     graph = Graph()  # Instantiate your graph
@@ -153,40 +277,40 @@ if __name__ == '__main__':
     Should print:
         {1: {2}, 2: {3, 4}, 3: {5}, 4: {6, 7}, 5: {3}, 6: {3}, 7: {1, 6}}
     '''
-    print(graph.vertices)
+    # print(graph.vertices)
 
-    '''
-    Valid BFT paths:
-        1, 2, 3, 4, 5, 6, 7
-        1, 2, 3, 4, 5, 7, 6
-        1, 2, 3, 4, 6, 7, 5
-        1, 2, 3, 4, 6, 5, 7
-        1, 2, 3, 4, 7, 6, 5
-        1, 2, 3, 4, 7, 5, 6
-        1, 2, 4, 3, 5, 6, 7
-        1, 2, 4, 3, 5, 7, 6
-        1, 2, 4, 3, 6, 7, 5
-        1, 2, 4, 3, 6, 5, 7
-        1, 2, 4, 3, 7, 6, 5
-        1, 2, 4, 3, 7, 5, 6
-    '''
-    graph.bft(1)
+    # '''
+    # Valid BFT paths:
+    #     1, 2, 3, 4, 5, 6, 7
+    #     1, 2, 3, 4, 5, 7, 6
+    #     1, 2, 3, 4, 6, 7, 5
+    #     1, 2, 3, 4, 6, 5, 7
+    #     1, 2, 3, 4, 7, 6, 5
+    #     1, 2, 3, 4, 7, 5, 6
+    #     1, 2, 4, 3, 5, 6, 7
+    #     1, 2, 4, 3, 5, 7, 6
+    #     1, 2, 4, 3, 6, 7, 5
+    #     1, 2, 4, 3, 6, 5, 7
+    #     1, 2, 4, 3, 7, 6, 5
+    #     1, 2, 4, 3, 7, 5, 6
+    # '''
+    # graph.bft(1)
 
-    '''
-    Valid DFT paths:
-        1, 2, 3, 5, 4, 6, 7
-        1, 2, 3, 5, 4, 7, 6
-        1, 2, 4, 7, 6, 3, 5
-        1, 2, 4, 6, 3, 5, 7
-    '''
-    graph.dft(1)
+    # '''
+    # Valid DFT paths:
+    #     1, 2, 3, 5, 4, 6, 7
+    #     1, 2, 3, 5, 4, 7, 6
+    #     1, 2, 4, 7, 6, 3, 5
+    #     1, 2, 4, 6, 3, 5, 7
+    # '''
+    # graph.dft(1)
     # graph.dft_recursive(1)
 
-    # '''
-    # Valid BFS path:
-    #     [1, 2, 4, 6]
-    # '''
-    # print(graph.bfs(1, 6))
+    '''
+    Valid BFS path:
+        [1, 2, 4, 6]
+    '''
+    print(graph.bfs(1, 6))
 
     # '''
     # Valid DFS paths:
