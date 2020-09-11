69fd44c209d0c5682dcc7fdcde1b6a4e1dd36a12
David Molinari <david.m.molinari@gmail.com>
2020-09-11 12:27:43 -0500

    Sprint progress

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -2,8 +2,98 @@ from room import Room
 from player import Player
 from world import World
 
+from util import Stack, Queue
+
 import random
 from ast import literal_eval
+
+class Graph:
+
+    """Represent a graph as a dictionary of vertices mapping labels to edges."""
+    def __init__(self):
+        self.vertices = {}
+
+    def add_vertex(self, vertex_id):
+        """
+        Add a vertex to the graph.
+        """
+        # pass  # TODO
+        self.vertices[vertex_id] = {}
+        
+
+    def add_edge(self, v1, v2):
+        """
+        Add a directed edge to the graph.
+        """
+        # pass  # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
+
+    def get_neighbors(self, vertex_id):
+        """
+        Get all neighbors (edges) of a vertex.
+        """
+        # pass  # TODO
+        return self.vertices[vertex_id]
+
+    def bft(self, starting_vertex):
+        """
+        Print each vertex in breadth-first order
+        beginning from starting_vertex.
+        """
+        # pass  # TODO
+        search_tracker = {}
+
+        for v in self.vertices:
+            search_tracker[v] = 0
+
+        q = Queue()
+
+        search_tracker[starting_vertex] = 1
+        q.enqueue(starting_vertex)
+
+        while q.isEmpty() is not True:
+            u = q.getOne(0)
+            
+            for v in self.get_neighbors(u):
+                if search_tracker[v] == 0:
+                    search_tracker[v] = 1
+                    q.enqueue(v)
+            
+            print(u)
+            q.dequeue()
+            search_tracker[u] = 2
+
+    def dft(self, starting_vertex):
+        """
+        Print each vertex in depth-first order
+        beginning from starting_vertex.
+        """
+        # pass  # TODO
+
+        search_tracker = {}
+        parent_tracker = {}
+
+        def DFS_visit(vert):
+            search_tracker[vert] = 1
+            print(vert)
+
+            for n in self.get_neighbors(vert):
+                if search_tracker[n] == 0:
+                    parent_tracker[n] = vert
+                    DFS_visit(n)
+
+            search_tracker[vert] = 2
+
+        for v in self.vertices:
+            search_tracker[v] = 0
+            parent_tracker[v] = None
+
+        for v in self.vertices:
+            if search_tracker[v] == 0:
+                DFS_visit(v)
 
 # Load world
 world = World()
@@ -14,7 +104,7 @@ world = World()
 # map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+map_file = "projects/adventure/maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -29,7 +119,34 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+graph = Graph()
 
+current_room = player.current_room.id
+
+def travel_function(current_room):
+    current_exits = player.current_room.get_exits()
+    if graph.vertices[current_room] is None:
+        graph.add_vertex(current_room)
+        print(current_room, current_exits)
+        for i in current_exits:
+            graph.vertices[current_room][i] = '?'
+            print(graph.vertices[current_room])
+    for i in graph.vertices[current_room]:
+        if graph.vertices[current_room][i] != int:
+            direction = i
+            print(direction)
+            old_room = current_room
+            player.travel(direction)
+            print(player.current_room.id)
+            graph.vertices[old_room][direction] = player.current_room.id
+            travel_function(player.current_room.id)
+            break
+    for i in graph.vertices[current_room]:
+            
+
+
+
+travel_function(current_room)
 
 # TRAVERSAL TEST
 visited_rooms = set()
---
+++ projects/adventure/util.py
@@ -0,0 +1,36 @@
+
+# Note: This Queue class is sub-optimal. Why?
+class Queue():
+    def __init__(self):
+        self.queue = []
+    def enqueue(self, value):
+        self.queue.append(value)
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+    def size(self):
+        return len(self.queue)
+    def isEmpty(self):
+        if len(self.queue) > 0:
+            return False
+        else:
+            return True
+    def getOne(self, index):
+        if self.isEmpty() is False:
+            return self.queue[index]
+
+class Stack():
+    def __init__(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+    def size(self):
+        return len(self.stack)
+
